48491f47057119c62a90c27ba0f88e1933fbefeb
OriShefer <ori150403@gmail.com>
2023-04-26 13:01:40 +0300

    Add fragment and portals to error

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,7 +3,9 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
+
 import classes from './AddUser.module.css';
+
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
