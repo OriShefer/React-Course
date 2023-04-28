d4f81093c2cc023ed5d7ac50a414491a324408cf
OriShefer <ori150403@gmail.com>
2023-04-28 23:42:09 +0300

    Section-12

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback, useMemo } from 'react';
 
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
+import './App.css';
+import DemoList from './components/Demo/DemoList';
+import Button from './components/UI/Button/Button';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [listTitle, setListTitle] = useState('My List');
 
-  useEffect(() => {
-    const userLogedInInformation = localStorage.getItem("isLoggedIn");
-
-    if (userLogedInInformation === "1") {
-      setIsLoggedIn(true);
-    }
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
   }, []);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem("isLoggedIn", "1");
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.setItem("isLoggedIn", "0");
-    setIsLoggedIn(false);
-  };
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+    <div className="app">
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
+    </div>
   );
 }
 
